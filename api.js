92359229928558aed8fbd333550b45b7e3e4e843
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-07-28 02:52:29 -0300

    First Commit

---
+++ api.js
@@ -0,0 +1,328 @@
+const api = {
+    "globals": {
+        "__SRC_ICON_PERSPECTIVE_VIEW": "https://img.icons8.com/nolan/64/perspective-view.png"
+    },
+    "header": {
+        "fields": {
+            "Row Number": {},
+            "First Name": {},
+            "Last Name": {},
+            "Email Adress": {},
+            "Gender": {},
+            "Car Make": {},
+            "Color": {},
+            "Domain Name": {},
+            "EIN": {},
+            "MD5": {}
+        },
+        "title": `de Finibus Bonorum et Malorum`
+    },
+    "body": {
+        "dataset": [
+            {
+                "id": 1,
+                "first_name": "Ammamaria",
+                "last_name": "Gemlbett",
+                "email": "agemlbett0@rakuten.co.jp",
+                "gender": "Female",
+                "car_make": "Pontiac",
+                "color": "Aquamarine",
+                "domain_name": "bandcamp.com",
+                "employer_identification_number": "34-8345090",
+                "md5": "aa1cd313169984958ca2fa65c22ce10e"
+            },
+            {
+                "id": 2,
+                "first_name": "Prentiss",
+                "last_name": "Huff",
+                "email": "phuff1@themeforest.net",
+                "gender": "Male",
+                "car_make": "Volvo",
+                "color": "Maroon",
+                "domain_name": "blogger.com",
+                "employer_identification_number": "77-2674339",
+                "md5": "d090db985483c67a4dcb82ab31533402"
+            },
+            {
+                "id": 3,
+                "first_name": "Tyson",
+                "last_name": "Gorcke",
+                "email": "tgorcke2@yolasite.com",
+                "gender": "Male",
+                "car_make": "Pontiac",
+                "color": "Yellow",
+                "domain_name": "youtube.com",
+                "employer_identification_number": "69-9730759",
+                "md5": "bd6150cbc1ea8d3ea648473082f6fc45"
+            },
+            {
+                "id": 4,
+                "first_name": "Annabella",
+                "last_name": "Ketchaside",
+                "email": "aketchaside3@berkeley.edu",
+                "gender": "Female",
+                "car_make": "Mercedes-Benz",
+                "color": "Goldenrod",
+                "domain_name": "paginegialle.it",
+                "employer_identification_number": "63-3336342",
+                "md5": "d60cbcc63c2e3bae0f2073ad201ebb21"
+            },
+            {
+                "id": 5,
+                "first_name": "Lida",
+                "last_name": "Choke",
+                "email": "lchoke4@squidoo.com",
+                "gender": "Female",
+                "car_make": "Pontiac",
+                "color": "Blue",
+                "domain_name": "webeden.co.uk",
+                "employer_identification_number": "39-7644387",
+                "md5": "0a4b75f892462efcf5699b742a64e7a7"
+            },
+            {
+                "id": 6,
+                "first_name": "Roxanna",
+                "last_name": "McRitchie",
+                "email": "rmcritchie5@uol.com.br",
+                "gender": "Female",
+                "car_make": "Ford",
+                "color": "Goldenrod",
+                "domain_name": "tripadvisor.com",
+                "employer_identification_number": "51-8525032",
+                "md5": "fd6c836410d085490f098dffd98fc72e"
+            },
+            {
+                "id": 7,
+                "first_name": "Ignatius",
+                "last_name": "Gramer",
+                "email": "igramer6@intel.com",
+                "gender": "Male",
+                "car_make": "Land Rover",
+                "color": "Indigo",
+                "domain_name": "goodreads.com",
+                "employer_identification_number": "79-7513213",
+                "md5": "0ec85bc5ed14f3478788f9e19d873f05"
+            },
+            {
+                "id": 8,
+                "first_name": "Collen",
+                "last_name": "Fetherstone",
+                "email": "cfetherstone7@netscape.com",
+                "gender": "Female",
+                "car_make": "Kia",
+                "color": "Aquamarine",
+                "domain_name": "google.de",
+                "employer_identification_number": "05-4992311",
+                "md5": "ee170cdf12deb015779f6a661372318a"
+            },
+            {
+                "id": 9,
+                "first_name": "Cammy",
+                "last_name": "Elven",
+                "email": "celven8@wufoo.com",
+                "gender": "Male",
+                "car_make": "Isuzu",
+                "color": "Indigo",
+                "domain_name": "mtv.com",
+                "employer_identification_number": "94-4031272",
+                "md5": "f74a35213fbd7b9ff7555a9646b52958"
+            },
+            {
+                "id": 10,
+                "first_name": "Austin",
+                "last_name": "Bucher",
+                "email": "abucher9@digg.com",
+                "gender": "Female",
+                "car_make": "BMW",
+                "color": "Violet",
+                "domain_name": "google.ru",
+                "employer_identification_number": "92-8716085",
+                "md5": "51edd77e39d73fa3d944f51b7fd98dda"
+            },
+            {
+                "id": 11,
+                "first_name": "Iseabal",
+                "last_name": "Stronack",
+                "email": "istronacka@washingtonpost.com",
+                "gender": "Female",
+                "car_make": "Buick",
+                "color": "Orange",
+                "domain_name": "domainmarket.com",
+                "employer_identification_number": "62-0827292",
+                "md5": "e55b2ea5e11a6c7d8b4aaca9144fa866"
+            },
+            {
+                "id": 12,
+                "first_name": "Padraic",
+                "last_name": "Gilsthorpe",
+                "email": "pgilsthorpeb@a8.net",
+                "gender": "Male",
+                "car_make": "Jeep",
+                "color": "Yellow",
+                "domain_name": "360.cn",
+                "employer_identification_number": "70-9536801",
+                "md5": "4a87bc3b7499affe1f4da963fb820823"
+            },
+            {
+                "id": 13,
+                "first_name": "Barnett",
+                "last_name": "Lilleycrop",
+                "email": "blilleycropc@weather.com",
+                "gender": "Male",
+                "car_make": "Lexus",
+                "color": "Maroon",
+                "domain_name": "instagram.com",
+                "employer_identification_number": "87-8740231",
+                "md5": "d3b3dbff6bcd23642895c6817bc74de5"
+            },
+            {
+                "id": 14,
+                "first_name": "Noel",
+                "last_name": "Threadkell",
+                "email": "nthreadkelld@theatlantic.com",
+                "gender": "Female",
+                "car_make": "Dodge",
+                "color": "Turquoise",
+                "domain_name": "usgs.gov",
+                "employer_identification_number": "43-3994514",
+                "md5": "ca4feb9c38e5e6f217209b6d57c1c83d"
+            },
+            {
+                "id": 15,
+                "first_name": "Ara",
+                "last_name": "Roman",
+                "email": "aromane@seattletimes.com",
+                "gender": "Male",
+                "car_make": "Scion",
+                "color": "Mauv",
+                "domain_name": "bigcartel.com",
+                "employer_identification_number": "62-4890277",
+                "md5": "3c31003551a8c21bcdea6828559a79f8"
+            },
+            {
+                "id": 16,
+                "first_name": "Jerome",
+                "last_name": "Lindstedt",
+                "email": "jlindstedtf@vkontakte.ru",
+                "gender": "Male",
+                "car_make": "Volvo",
+                "color": "Fuscia",
+                "domain_name": "edublogs.org",
+                "employer_identification_number": "36-1237693",
+                "md5": "a7b9c41f7ef2d8c800c30e44e37287c6"
+            },
+            {
+                "id": 17,
+                "first_name": "Juliann",
+                "last_name": "Cardoe",
+                "email": "jcardoeg@imageshack.us",
+                "gender": "Female",
+                "car_make": "MINI",
+                "color": "Crimson",
+                "domain_name": "comsenz.com",
+                "employer_identification_number": "13-6256322",
+                "md5": "1d0b6a59a6190d52b1166f14b9bad70a"
+            },
+            {
+                "id": 18,
+                "first_name": "Alberta",
+                "last_name": "Akerman",
+                "email": "aakermanh@multiply.com",
+                "gender": "Female",
+                "car_make": "Mazda",
+                "color": "Aquamarine",
+                "domain_name": "usgs.gov",
+                "employer_identification_number": "64-4631765",
+                "md5": "962895eeaed676e9e42474caef4b24b4"
+            },
+            {
+                "id": 19,
+                "first_name": "Pearce",
+                "last_name": "Cuseck",
+                "email": "pcusecki@tripadvisor.com",
+                "gender": "Male",
+                "car_make": "MINI",
+                "color": "Crimson",
+                "domain_name": "cbslocal.com",
+                "employer_identification_number": "94-5435996",
+                "md5": "a6867a105af2d8669a809281fac05adf"
+            },
+            {
+                "id": 20,
+                "first_name": "Hollie",
+                "last_name": "Kirmond",
+                "email": "hkirmondj@hhs.gov",
+                "gender": "Female",
+                "car_make": "Saab",
+                "color": "Puce",
+                "domain_name": "shop-pro.jp",
+                "employer_identification_number": "26-1979781",
+                "md5": "f390224124df1bb727680a93e18b0428"
+            },
+            {
+                "id": 21,
+                "first_name": "Diarmid",
+                "last_name": "Mirfin",
+                "email": "dmirfink@liveinternet.ru",
+                "gender": "Male",
+                "car_make": "Ford",
+                "color": "Blue",
+                "domain_name": "spotify.com",
+                "employer_identification_number": "70-2944254",
+                "md5": "7b25e2575da9dfeef41d6f701464e6d4"
+            },
+            {
+                "id": 22,
+                "first_name": "Josepha",
+                "last_name": "Bridgeland",
+                "email": "jbridgelandl@discuz.net",
+                "gender": "Female",
+                "car_make": "GMC",
+                "color": "Violet",
+                "domain_name": "newsvine.com",
+                "employer_identification_number": "23-0518141",
+                "md5": "e29de9669fd6cbb3ce9082b72e669050"
+            },
+            {
+                "id": 23,
+                "first_name": "Padriac",
+                "last_name": "Marchington",
+                "email": "pmarchingtonm@last.fm",
+                "gender": "Male",
+                "car_make": "Chevrolet",
+                "color": "Mauv",
+                "domain_name": "shop-pro.jp",
+                "employer_identification_number": "72-9719019",
+                "md5": "aa95fcd9a79bc61a00f7079e4116eee3"
+            },
+            {
+                "id": 24,
+                "first_name": "Kay",
+                "last_name": "Stoate",
+                "email": "kstoaten@pen.io",
+                "gender": "Female",
+                "car_make": "Mercury",
+                "color": "Pink",
+                "domain_name": "yale.edu",
+                "employer_identification_number": "32-7510679",
+                "md5": "ecfaea60f2e726f30f99d70fdcba1783"
+            },
+            {
+                "id": 25,
+                "first_name": "Joye",
+                "last_name": "Caveney",
+                "email": "jcaveneyo@live.com",
+                "gender": "Female",
+                "car_make": "BMW",
+                "color": "Violet",
+                "domain_name": "baidu.com",
+                "employer_identification_number": "65-0036610",
+                "md5": "ef9747e7b76cacfab093a4d81d20c83f"
+            }
+        ]
+    },
+    "footer": {
+        "pdf": true,
+        "csv": true
+    }
+}
